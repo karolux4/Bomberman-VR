e927bc9549ddc784680e6faf7d37479506390f69
karolux4 <karolisbutkus4@gmail.com>
2019-02-04 08:21:59 +0200

    Camera rotation

--- Bomberman/Assets/Code/Player/CameraRotation.cs
+++ Bomberman/Assets/Code/Player/CameraRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
 
+    public GameObject Main_Camera_Control;
     public Vector2 prev_mouse_pos;
     Vector2 smooth;
     public float sensitivity;
@@ -14,7 +15,7 @@ public class CameraRotation : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        player = this.transform.parent.gameObject;
+        player = this.transform.parent.gameObject.transform.parent.gameObject;
         prev_mouse_pos = new Vector2(player.transform.eulerAngles.y, 0);
 	}
 	
@@ -22,9 +23,9 @@ public class CameraRotation : MonoBehaviour {
 	void Update ()
     {
         sensitivity = GameObject.Find("Sensitivity").GetComponent<Sensitivity_Value>().Sensitivity;
-        if ((Time.timeScale != 0)&&(this.gameObject.transform.localPosition.y<3))
+        if ((Time.timeScale != 0)&&(Main_Camera_Control.transform.localPosition.y<3))
         {
-            var mouse_delta = new Vector2(Input.GetAxisRaw(MouseX), Input.GetAxisRaw(MouseY));
+            var mouse_delta = new Vector2(Input.GetAxisRaw(MouseX), 0f);
             mouse_delta = Vector2.Scale(mouse_delta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
             smooth.x = Mathf.Lerp(smooth.x, mouse_delta.x, 1f / smoothing);
             smooth.y = Mathf.Lerp(smooth.y, mouse_delta.y, 1f / smoothing);
@@ -37,7 +38,7 @@ public class CameraRotation : MonoBehaviour {
             {
                 prev_mouse_pos.y = -75;
             }
-            transform.localRotation = Quaternion.AngleAxis(-prev_mouse_pos.y, Vector3.right);
+            Main_Camera_Control.transform.localRotation = Quaternion.AngleAxis(-prev_mouse_pos.y, Vector3.right);
             player.transform.localRotation = Quaternion.AngleAxis(prev_mouse_pos.x, player.transform.up);
         }
 	}
